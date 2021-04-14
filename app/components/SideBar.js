e31bcbddc20bb84c1eb41824b81f4651f1ba70f5
Kevin-KHA <201378@ids.upchiapas.edu.mx>
2021-04-13 21:26:48 -0500

    Se agrega sidebar

---
+++ app/components/SideBar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Link} from "react-router-dom";
+class SideBar extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render(){
+        return (
+            <React.Fragment>
+                <div className="modal-dialog ">
+
+                </div>
+            </React.Fragment>
+        )
+    }
+};
+
+export default SideBar;
+
